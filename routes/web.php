5973981613ba2d5d2bf61ba5c70b7c35ea188437
jarotsujadmiko <jarot.sujadmiko@gmail.com>
2021-04-14 01:27:03 +0700

    menambah konfigurasi routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AuthController::class,'showFormLogin'])->name('login');
+Route::get('login',[AuthController::class,'showFormLogin'])->name('login');
+Route::post('login',[AuthController::class,'login']);
+Route::get('register',[AuthController::class,'showFormRegister'])->name('register');
+Route::post('register',[AuthController::class,'register']);
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('home',[HomeController::class,'index'])->name('home');
+    Route::get('logout',[AuthController::class,'logout'])->name('logout');
+}
+);
+
